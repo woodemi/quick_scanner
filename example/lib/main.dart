de0e6f30a980c276f21f344d775d512477ee2382
wangkun <wangkun@woodemi.com>
2021-06-18 22:18:31 +0800

    Add startWatch & stopWatch
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:quick_scanner/quick_scanner.dart';
 
 void main() {
@@ -14,36 +12,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await QuickScanner.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -51,8 +19,26 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  child: Text('startWatch'),
+                  onPressed: () async {
+                    QuickScanner.startWatch();
+                  },
+                ),
+                ElevatedButton(
+                  child: Text('stopWatch'),
+                  onPressed: () async {
+                    QuickScanner.stopWatch();
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
--- lib/quick_scanner.dart
+++ lib/quick_scanner.dart
@@ -11,4 +11,8 @@ class QuickScanner {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
+
+  static Future<void> startWatch() => _channel.invokeMethod('startWatch');
+
+  static Future<void> stopWatch() => _channel.invokeMethod('stopWatch');
 }
