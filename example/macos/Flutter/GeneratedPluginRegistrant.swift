dcad70f4aa3dad20529082b48ff92e827f0b1abf
wangkun <wangkun@woodemi.com>
2021-06-18 20:09:27 +0800

    Add macOS project
    Signed-off-by: wangkun <wangkun@woodemi.com>

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import quick_scanner
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  QuickScannerPlugin.register(with: registry.registrar(forPlugin: "QuickScannerPlugin"))
+}
